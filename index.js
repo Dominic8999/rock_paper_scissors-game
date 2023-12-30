03cee887c284ec502013e17f2e6e6d22ab23672e
Dominic8999 <dominic3331@fedora.email>
2023-12-30 17:37:35 +0100

    base game, yet still kinda broken

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 let tools = ["rock", "paper", "scissors"]
 
-let random_number = Math.floor(Math.random() * 2) + 1
-chose
+let random_number = Math.floor(Math.random() * 3)
+let computer_choice = tools[random_number]
 
 console.log(random_number)
+console.log(computer_choice)
 
-function game(){
-    switch(tools){
-        case tools === "rock":
+let player_choice = prompt("Choose your tool: rock, paper or scissors.")
 
+function game(player_choice){
+    let message = undefined
+    if (computer_choice > player_choice){
+        message = "The computer has won!"
+        return message
     }
+    if (computer_choice < player_choice){
+        message = "You've won the game!"
+    }
+    else{
+        message = "draw"
+        return message
+    }
+    console.log(message)
 }
+
+game(player_choice)
