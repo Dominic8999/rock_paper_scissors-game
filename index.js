bc2de5837efa28f33ab4a05bf5f9caa7be25a5e6
Dominic8999 <dominic3331@fedora.email>
2023-12-30 18:20:57 +0100

    The game works fine! (somehow)

--- index.js
+++ index.js
@@ -8,20 +8,32 @@ console.log(computer_choice)
 
 let player_choice = prompt("Choose your tool: rock, paper or scissors.")
 
-function game(player_choice){
-    let message = undefined
-    if (computer_choice > player_choice){
-        message = "The computer has won!"
-        return message
+// function check(player_choice){
+//     // if (player_choice === ""){
+//     //         return alert("please write some tool")
+//     // }
+//     // // if (player_choice != "rock") {
+//     // //     choice = player_choice
+//     // //     return choice && alert("please write it correctly!") 
+//     // // }
+    // else{
+        function game(){
+            let message = undefined
+            if (computer_choice == "rock" && player_choice == "scissors" || computer_choice == "paper" && player_choice == "rock"
+             || computer_choice == "scissors" && player_choice == "paper"){
+                message = alert("The computer has won!")
+                return message
+            }
+            if (player_choice == "rock" && computer_choice == "scissors" || player_choice == "paper" && computer_choice == "rock"
+            || player_choice == "scissors" && computer_choice == "paper"){
+                message = alert("You've won the game!")
+            }
+            if (computer_choice == player_choice){
+                message = alert("draw")
+                return message
+            }
+            console.log(message)
+        // }
     }
-    if (computer_choice < player_choice){
-        message = "You've won the game!"
-    }
-    else{
-        message = "draw"
-        return message
-    }
-    console.log(message)
-}
-
-game(player_choice)
+// }
+game()
