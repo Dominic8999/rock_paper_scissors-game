9b68cc09477e984505b44a7f24109322e5a95b7c
Dominic8999 <dominic3331@fedora.email>
2023-12-30 19:49:39 +0100

    correct the index.js file

--- index.js
+++ index.js
@@ -8,32 +8,22 @@ console.log(computer_choice)
 
 let player_choice = prompt("Choose your tool: rock, paper or scissors.")
 
-// function check(player_choice){
-//     // if (player_choice === ""){
-//     //         return alert("please write some tool")
-//     // }
-//     // // if (player_choice != "rock") {
-//     // //     choice = player_choice
-//     // //     return choice && alert("please write it correctly!") 
-//     // // }
-    // else{
-        function game(){
-            let message = undefined
-            if (computer_choice == "rock" && player_choice == "scissors" || computer_choice == "paper" && player_choice == "rock"
-             || computer_choice == "scissors" && player_choice == "paper"){
-                message = alert("The computer has won!")
-                return message
-            }
-            if (player_choice == "rock" && computer_choice == "scissors" || player_choice == "paper" && computer_choice == "rock"
-            || player_choice == "scissors" && computer_choice == "paper"){
-                message = alert("You've won the game!")
-            }
-            if (computer_choice == player_choice){
-                message = alert("draw")
-                return message
-            }
-            console.log(message)
-        // }
+function game(){
+    let message = undefined
+    if (computer_choice == "rock" && player_choice == "scissors" || computer_choice == "paper" && player_choice == "rock"
+    || computer_choice == "scissors" && player_choice == "paper"){
+        message = alert("The computer has won!")
+        return message
     }
-// }
+    if (player_choice == "rock" && computer_choice == "scissors" || player_choice == "paper" && computer_choice == "rock"
+    || player_choice == "scissors" && computer_choice == "paper"){
+        message = alert("You've won the game!")
+    }
+    if (computer_choice == player_choice){
+        message = alert("draw")
+        return message
+    }
+    console.log(message)
+    alert(message)
+    }
 game()
