5b2b4cfd6428ce6ad22c4852a409ee8c6eeffc16
Dominic8999 <dominic3331@fedora.email>
2023-12-30 17:17:32 +0100

    add index.js && index.html

---
+++ index.js
@@ -0,0 +1,13 @@
+let tools = ["rock", "paper", "scissors"]
+
+let random_number = Math.floor(Math.random() * 2) + 1
+chose
+
+console.log(random_number)
+
+function game(){
+    switch(tools){
+        case tools === "rock":
+
+    }
+}
