a1ed80489f8b811646c8cac2bfb33ab8085e2b31
Dominic8999 <dominic3331@fedora.email>
2024-01-01 12:57:20 +0200

    fix the game; fix catching errors; fix alert messages

--- index.js
+++ index.js
@@ -10,39 +10,40 @@ let player_choice = prompt("Choose your tool: rock, paper or scissors.")
 
 
 function game(){
-    let message = console.info("deafult message")
     if (computer_choice == "rock" && player_choice == "scissors" || computer_choice == "paper" && player_choice == "rock"
     || computer_choice == "scissors" && player_choice == "paper"){
-        message = alert("The computer has won!")
+        message = `The computer has won! The computer used ${computer_choice}, and you used ${player_choice}`
+        alert(message)
+        console.log(message)
         return message
     }
     if (player_choice == "rock" && computer_choice == "scissors" || player_choice == "paper" && computer_choice == "rock"
     || player_choice == "scissors" && computer_choice == "paper"){
-        message = alert("You've won the game!")
-    }
-    if (computer_choice == player_choice){
-        message = alert("draw")
+        message = `You've won the game! You've used ${player_choice}, and the computer used ${computer_choice}.`
+        alert(message)
+        console.log(message)
         return message
     }
-    alert(message)
-    console.log(message)        
+    if (computer_choice == player_choice){
+        message = `It's a draw! The computer used ${computer_choice}, and you used ${player_choice} at the same time!`
+        alert(message)
+        console.log(message)
+        return message
+    }
 }
 function check(){
     if (player_choice == ""){
         let warning = alert("You typed no tool whatsoever :(")
+        console.log("The user typed no words at all :(")
         return warning
     }
     else{
-        try{
-            game()
-        }
-        catch(e){
-            let error = console.error(`${e} is supposed to happened if end user typed one of the tool correctly`)
-            return error
-        }
+        game()
     }
-    console.warn(warning)
-    alert(warning)
 }
-check()
-game()
+try{
+    check()
+}
+catch(e){
+    console.info(`"${e}" error is supposed to happened if end user typed one of the tool correctly`)
+}
